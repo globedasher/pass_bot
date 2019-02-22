e016a78e3279bcd91e7ffc3d0bba72366a64b908
trainwrek <globedasher>
2019-02-21 21:20:45 -0800

    Added arguments handler and test argument.

--- bot.py
+++ bot.py
@@ -1,55 +1,66 @@
-import sys, os, logging, json, requests
+import sys, os, logging, json, requests, argparse
 import urllib.request
-from twitter import *
+import twitter
 
 # This is your key file to keep your API id and such.
 import key
 
-def config():
+def config(log_level):
+    # Get the log level and set the parameter for logging messages
+    if log_level == "debug":
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    # Configure the format for the log messages
     formatter = '%(asctime)s: %(levelname)s: %(message)s'
+
+    # Configure logging
     logging.basicConfig(format=formatter,
             filename='debug.log',
             filemode='w',
-            level=logging.DEBUG)
+            level=log_level)
+
+    # Log that
     logging.info("Logging initialized.")
 
 def all_passes():
     #Get information all passes
     all_passes_url = "https://wsdot.com/Traffic/api/MountainPassConditions/MountainPassConditionsREST.svc/GetMountainPassConditionsAsJson?AccessCode={%s}" % (key.wa_access_code)
     logging.info("All passes request")
-    logging.info(all_passes_url)
+    logging.debug(all_passes_url)
     print(all_passes_url)
 
     response = urllib.request.urlopen(all_passes_url)
     data = json.loads(response.read().decode("utf-8"))
     print(data[0])
-    logging.info(data[0])
+    logging.debug(data[0])
     print(str(data[0]["TemperatureInFahrenheit"]) + " degrees")
-    logging.info(str(data[0]["TemperatureInFahrenheit"]) + " degrees")
+    logging.debug(str(data[0]["TemperatureInFahrenheit"]) + " degrees")
     #print(type(data))
 
     #for item in data:
         #print(item)
         #print(type(item))
-        #logging.info(item)
+        #logging.debug(item)
 
     #print(data)
     #print(dir(data))
-    #logging.info(str(data))
-    logging.info("ouch")
+    #logging.debug(str(data))
+    logging.debug("ouch")
 
 def one_from_all_passes(pass_id):
     #Get information all passes
     logging.info("One from all passes request")
     all_passes_url = "https://wsdot.com/Traffic/api/MountainPassConditions/MountainPassConditionsREST.svc/GetMountainPassConditionsAsJson?AccessCode={%s}" % (key.wa_access_code)
-    logging.info(all_passes_url)
+    logging.debug(all_passes_url)
     #print(all_passes_url)
 
     other_response = requests.get(all_passes_url)
     response = json.loads(other_response.content.decode("utf-8"))
     data = response
 
-    logging.info(dir(response))
+    logging.debug(dir(response))
     #data = json.loads(response.read().decode("utf-8"))
     logging.info(data[pass_id])
     post_string = ""
@@ -68,7 +79,7 @@ def one_from_all_passes(pass_id):
         post_string += "W: "
         post_string += rest_two
     if rest_one == rest_two:
-        post_string += rest_one + " either direction"
+        post_string += rest_one
     return post_string
 
 
@@ -80,8 +91,8 @@ def get_camera(camera_id):
 
     camera_url = "http://wsdot.wa.gov/Traffic/api/HighwayCameras/HighwayCamerasREST.svc/GetCamerasAsJson?AccessCode={%s}" % (key.wa_access_code)
     log_message = "Getting image from camera #%s" % (camera_id)
-    logging.info(log_message)
-    logging.info(camera_url)
+    logging.debug(log_message)
+    logging.debug(camera_url)
 
     response = urllib.request.urlopen(camera_url)
     data = json.loads(response.read().decode("utf-8"))
@@ -91,7 +102,7 @@ def get_camera(camera_id):
         if message["CameraID"] == camera_id:
             image_url = message["ImageURL"]
 
-    logging.info(image_url)
+    logging.debug(image_url)
     return image_url
 
 
@@ -100,74 +111,78 @@ def one_pass():
     one_pass = "https://wsdot.com/Traffic/api/MountainPassConditions/MountainPassConditionsREST.svc/GetMountainPassConditionAsJon?AccessCode={%s}&PassConditionID={%s}" % (key.wa_access_code, 3)
     logging.info("One pass request")
     print(one_pass)
-    logging.info(one_pass)
+    logging.debug(one_pass)
     response = urllib.request.urlopen(one_pass)
     data = json.loads(response.read().decode("utf-8"))
     print(data)
-    #logging.info(str(data))
-    logging.info("ouch")
+    #logging.debug(str(data))
+    logging.debug("ouch")
 
 
 def twitter_post(post_data, image_url=0):
     logging.info("Twitter post attempt.")
-    if image_url:
-        auth = OAuth(
-                key.token,
-                key.token_secret,
-                key.consumer_key,
-                key.consumer_secret,
-                )
 
-        t_upload = Twitter(
-                domain="upload.twitter.com",
-                auth=auth)
-
-        logging.info(image_url)
-        response = requests.get(image_url)
-        imagedata = response.content
-
-        image_url_1 = "https://images.wsdot.wa.gov/sc/090VC05347.jpg"
-        response1 = requests.get(image_url_1)
-        imagedata1 = response1.content
-
-        #id_img1 = t_upload.media.upload(media=imagedata)["media_id_string"]
-        id_img2 = t_upload.media.upload(media=imagedata1)["media_id_string"]
-
-    #print(id_img1, id_img2)
-
-
-    auth = OAuth(
-            key.token,
-            key.token_secret,
+    api = twitter.Api(
             key.consumer_key,
             key.consumer_secret,
-            )
-    twitter = Twitter(auth=auth)
-    logging.info(str(post_data))
-    #twitter.statuses.update(status=str(post_data))
-    twitter.statuses.update(status=str(post_data), media_ids=",".join([id_img2]))
-
-def post_test(post_data):
-    logging.info("Twitter post attempt.")
-    auth = OAuth(
             key.token,
             key.token_secret,
-            key.consumer_key,
-            key.consumer_secret,
             )
-    twitter = Twitter(auth=auth)
-    logging.info(str(post_data))
-    twitter.statuses.update(status=str(post_data))
+
+    logging.debug(post_data)
+    logging.debug(image_url)
+
+    media="https://images.wsdot.wa.gov/sc/090VC05347.jpg"
+    logging.debug(media)
+
+    api.PostUpdate(post_data, media)
+
+
+def get_args():
+    """
+    Get argument data passed from the command line and return a dictionary of
+    the arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    help_text = """The control selector can be set to either 'process' or 'post'. 'process' will run the main loop of the process. 'post' will post once and quit."""
+    parser.add_argument("-s"
+                        , "--selector"
+                        , dest="selector"
+                        , default=""
+                        , help=help_text
+                        )
+
+    help_text = """Set the log level to debug or not."""
+    parser.add_argument("-l"
+                        , "--loglevel"
+                        , dest="log_level"
+                        , default=""
+                        , help=help_text
+                        )
+
+    return parser.parse_args()
 
 
 def main():
-    config()
+
+    args = get_args()
+    #print(args)
+
+    if not args.selector:
+        print("No selectors?")
+        sys.exit(2)
+
+    # Configure the logging for this appliction.
+    config(args.log_level)
+
     #all_passes()
-    camera_image = get_camera(9715)
-    print(camera_image)
-    pass_report = one_from_all_passes(11)
-    print(pass_report)
-    twitter_post(pass_report, camera_image)
+    if args.selector == "post":
+        pass_report = one_from_all_passes(11)
+        twitter_post(pass_report)
+    elif args.selector == "test":
+        pass_report = one_from_all_passes(11)
+        logging.info(pass_report)
 
 if __name__ == "__main__":
     try:
